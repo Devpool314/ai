ef686cb7f9d325e5f2bf2bb20e6e80dba01fb17e
Hung <nohaimai610@hotmail.com>
2025-10-20 10:10:03 +0700

    Add gameplay and AI configuration settings
    Added gameplay and AI settings to the configuration.

--- settings.py
+++ settings.py
@@ -1,14 +1,19 @@
-# settings.py
-
-# Kích thước màn hình
 SCREEN_WIDTH = 850
 SCREEN_HEIGHT = 850
 
 # Màu sắc
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
+BLUE = (25, 25, 166)
 YELLOW = (255, 255, 0)
 
 # Kích thước của mỗi ô trong maze
 TILE_SIZE = 20
+
+# --- Cài đặt Gameplay ---
+MAZE_ROTATION_STEPS = 30       
+POWER_UP_DURATION = 5000  
+
+# --- Cài đặt AI ---
+AUTO_REPLAN_GHOST_DISTANCE = 4 
+
