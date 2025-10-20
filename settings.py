c7ff0bf0f86f9c8f2803f03c7e8d07a086782fd0
Hung <nohaimai610@hotmail.com>
2025-10-20 10:36:09 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,3 +17,9 @@ POWER_UP_DURATION = 5000
 # --- Cài đặt AI ---
 AUTO_REPLAN_GHOST_DISTANCE = 4 
 
+# --- MÀU MỚI ---
+DARK_WALL = (0, 0, 50)  # Màu tường sẫm hơn
+FOOD_COLOR = (255, 180, 180) # Màu đồ ăn hơi hồng
+POWER_UP_COLOR = (255, 255, 255) # Màu Power-up nổi bật
+STATUS_BAR_BG = (10, 10, 30) # Màu nền thanh trạng thái
+# -----------------
